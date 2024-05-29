8344267041198be5944031916153d62834e2c046
Lucas Moura <lucaspenha471@gmail.com>
2024-05-29 19:26:10 -0300

    Remove requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="rolf_common",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
-        "fastapi==0.109.1",
-        "pydantic==2.5.3",
-        "sqlalchemy[asyncio]==2.0.15"
     ],
 )
