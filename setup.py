c434b70ec731debb6b3a6715446e6aa2169e7d30
Lucas Moura <lucaspenha471@gmail.com>
2024-05-29 20:11:57 -0300

    Update __init__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rolf_common",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(include=['rolf_common', 'rolf_common.*']),
     install_requires=[
     ],
