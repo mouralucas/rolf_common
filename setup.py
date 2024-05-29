51bff6fd71fd658eef1cc8f1fdd51c42b5eb3208
Lucas Moura <lucaspenha471@gmail.com>
2024-05-29 20:04:20 -0300

    Add include to find_packages()

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="rolf_common",
     version="0.0.3",
-    packages=find_packages(),
+    packages=find_packages(include=['rolf_common', 'rolf_common.*']),
     install_requires=[
     ],
 )
