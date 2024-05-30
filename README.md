84af8f27b86793bd579906885cc19fcb40f5e6c7
Lucas Moura <lucaspenha471@gmail.com>
2024-05-29 21:24:10 -0300

    Add more info to set up and a simple readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rolf common pack
+
+This is the base code for all Rolf microservices.
+
+It implements the basic functions for several modules like 
+managers, models and services.
+
+Additional shared function are welcomed, but should not contain any business logic.
+
+This is still beta version, it's incomplete and not all functional.
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
+VERSION = '0.0.7'
+DESCRIPTION = 'Common package for all Rolf microservices'
+LONG_DESCRIPTION = 'This package contains all base classes for database managers, sql models, services and pydantic schemas'
+
 setup(
     name="rolf_common",
-    version="0.0.6",
+    author="Lucas Moura",
+    version=VERSION,
+    DESCRIPTION=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(include=['rolf_common', 'rolf_common.*']),
     install_requires=[
     ],
