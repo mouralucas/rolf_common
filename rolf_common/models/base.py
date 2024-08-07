9ed66ab0cae83bebb08ecd729a561e65409b2afe
Lucas Moura <lucaspenha471@gmail.com>
2024-08-07 14:38:00 -0300

    Adjust default value for created_at field in base sql model to a callable

--- rolf_common/models/base.py
+++ rolf_common/models/base.py
@@ -22,7 +22,7 @@ class SQLModel(Base):
 
     id: Mapped[uuid.UUID] = mapped_column('id', primary_key=True, default=uuid.uuid4)
     active: Mapped[bool] = mapped_column('active', default=True)
-    created_at: Mapped[datetime.datetime] = mapped_column('created_at', default=datetime.datetime.utcnow())
+    created_at: Mapped[datetime.datetime] = mapped_column('created_at', default=datetime.datetime.utcnow)
     edited_at: Mapped[datetime.datetime] = mapped_column('edited_at', nullable=True)
     deleted_at: Mapped[datetime.datetime] = mapped_column('deleted_at', nullable=True)
 
