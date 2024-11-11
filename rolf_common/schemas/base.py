6cfdda0d1b106a933fc3c7eb72660678ed0d4f44
Lucas Moura <lucaspenha471@gmail.com>
2024-11-11 15:57:52 -0300

    Create default pydantic model, to set config dict

--- rolf_common/schemas/base.py
+++ rolf_common/schemas/base.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, AliasGenerator
+from pydantic.alias_generators import to_camel, to_snake
 
 
 class SuccessResponseBase(BaseModel):
@@ -6,3 +7,12 @@ class SuccessResponseBase(BaseModel):
 
     success: bool = Field(True, serialization_alias='success')
     status_code: int = Field(None, serialization_alias='statusCode', json_schema_extra={"deprecated": True})
+
+
+class DefaultModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True,
+                              alias_generator=AliasGenerator(
+                                  alias=to_camel,
+                                  validation_alias=to_snake,
+                                  serialization_alias=to_camel,
+                              ))
