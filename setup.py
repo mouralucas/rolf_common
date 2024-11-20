d60717cddbe5427e4328696d928c60126c44d282
Lucas Moura <lucaspenha471@gmail.com>
2024-11-19 21:32:42 -0300

    Update common version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.36'
+VERSION = '0.0.37'
 DESCRIPTION = 'Common package for all Rolf microservices'
 LONG_DESCRIPTION = 'This package contains all base classes for database managers, sql models, services and pydantic schemas'
 
