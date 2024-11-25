ee80734b6fc783fdc6685f7b9e5b3e2a580d7170
Lucas Moura <lucaspenha471@gmail.com>
2024-11-25 10:21:37 -0300

    Add motor as required install dependency

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.41'
+VERSION = '0.0.42'
 DESCRIPTION = 'Common package for all Rolf microservices'
 LONG_DESCRIPTION = 'This package contains all base classes for database managers, sql models, services and pydantic schemas'
 
@@ -12,5 +12,6 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(include=['rolf_common', 'rolf_common.*']),
     install_requires=[
+        'motor'
     ],
 )
